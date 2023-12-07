7fcdbfcba4c0183efcd13919ff751e8b8dabddfd
juan.soares <juan.soares@darede.com.br>
2023-12-07 19:40:19 -0300

    DeepFlat

--- deepFlat.js
+++ deepFlat.js
@@ -3,6 +3,16 @@
  * @param {number} depth
  * @return {Array}
  */
+
 var flat = function (arr, n) {
-  return arr.flat(n);
+  if (n === 0) return arr;
+  const stack = [...arr.map((item) => [item, n])];
+  let temp = [];
+  while (stack.length > 0) {
+    const [item, n] = stack.pop();
+    if (Array.isArray(item) && n > 0)
+      stack.push(...item.map((subItem) => [subItem, n - 1]));
+    else temp.push(item);
+  }
+  return temp.reverse();
 };
